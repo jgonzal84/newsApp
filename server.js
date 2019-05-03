eb3de5b141d1ebbfc177b4767aa2abe17abf90a6
Joe Gonzalez <joseph.gonzalez111@gmail.com>
2019-05-02 21:33:50 -0700

    added promise to the call back

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //Dependencies
-var express =  require("express");
-var mongojs =  require("mongojs");
+var express = require("express");
+var mongojs = require("mongojs");
 // Require axios and cheerio. This makes the scraping possible
 var axios = require("axios");
 var cheerio = require("cheerio");
@@ -13,9 +13,9 @@ var databaseUrl = "scraper";
 var collections = ["scraperData"];
 
 // Hook Mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections); 
+var db = mongojs(databaseUrl, collections);
 db.on("error", function (error) {
-    console.log ("Database error: ", error);
+    console.log("Database error: ", error);
 });
 
 app.get("/", function (req, res) {
@@ -23,11 +23,12 @@ app.get("/", function (req, res) {
 });
 
 app.get("/all", function (req, res) {
-    db.scrapedData.find({}, function (error, found) {
+    db.scraperData.find({}, function (error, found) {
         if (error) {
             console.log(error);
         } else {
             res.json(found);
+            // res.render('pages/main', {respomse: response}); you can only have one response here at a time.
         }
     });
 });
@@ -35,44 +36,66 @@ app.get("/all", function (req, res) {
 app.get("/scrape", function (req, res) {
     axios.get("https://www.washingtonpost.com/opinions/?nid=top_nav_opinions&utm_term=.23f68f2edfd2").then(function (response) {
         var $ = cheerio.load(response.data);
+
+        var promises = [];
         $(".story-list-story").each(function (i, element) {
             var $el = $(element);
+            var title = $el.children().eq(0).children().eq(1).children().eq(1).text();
+            var image = $el.children().eq(1).children('a').attr('img');
+            var summary = $el.children().eq(0).children().eq(2).text();
+            var link = $el.children().eq(1).children('a').attr('href');
 
-            console.log("title: ", $el.children().eq(0).children().eq(1).children().eq(1).text()); //html());
-            console.log("link", $el.children().eq(1).children('a').attr('href'));
-            console.log("summary: ", $el.children().eq(0).children().eq(2).text()); //html());
-            console.log('---------')
-            console.log('---------')
-
-            var title = $(element).children('a').text();
-            var image = $(element).children('img').attr('src');
-            var summary = $(element).children('p').text();
-            var link = $(element).children('a').attr("href");
-
-            console.log(title, summary, link);
+            console.log("title", title)
+            console.log("title", image)
+            console.log("title", summary)
+            console.log("title", link)
 
             if (title && summary && link) {
-                db.scrapedData.insert({
-                    title: Title,
-                    summary: Summary,
-                    link: Link
-                },
-                function (err, inserted){
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log(inserted);
-                    }
-                });
+                var promise = myFunction(title, summary, link)
+                promises.push(promise)
             }
         })
-    })
-    res.send("All Scraped!")
+                Promise.all(promises)
+                    .then(function (result) {
+                        console.log(result) 
+                            res.render('pages/article_grid', { result });
+                        })
+                    });
 });
 
 app.listen(3001, function () {
-    console.log("running on 3001");
+        console.log("running on 3001");
+    });
+
+function myFunction(title, summary, link) {
+    return new Promise(function (resolve, reject) {
+        db.scraperData.insert({
+            title: title,
+            image: image,
+            summary: summary,
+            link: link
+        }, function (err, inserted) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(inserted);
+            }
+        }
+
+        
+                
+
+        res.send("All Scraped!")
+        // for rob the page was rendering too fast so he used promise method but still not working.
+        // Promise.all(promises)
+        // .then( function(result){
+        //     res.json(result)
+        //     // res.render('pages/news', {response});
+        // })
+        }
+
+    
+
+
 })
-
-
-
+}
