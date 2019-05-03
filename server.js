7b9582226f5f3432286cfdb7a9417992b526c276
Joe Gonzalez <joseph.gonzalez111@gmail.com>
2019-05-02 19:39:55 -0700

    new changes but still need to get rid of undefined coming back on nodemon. also need to add img to return

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express =  require("express");
 var mongojs =  require("mongojs");
 // Require axios and cheerio. This makes the scraping possible
 var axios = require("axios");
-var cheerio = requires("cheerio");
+var cheerio = require("cheerio");
 
 // Initialize express
 var app = express();
@@ -15,5 +15,64 @@ var collections = ["scraperData"];
 // Hook Mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections); 
 db.on("error", function (error) {
-    console.log ("Yippy!")
+    console.log ("Database error: ", error);
+});
+
+app.get("/", function (req, res) {
+    res.send("Ready!")
+});
+
+app.get("/all", function (req, res) {
+    db.scrapedData.find({}, function (error, found) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.json(found);
+        }
+    });
+});
+
+app.get("/scrape", function (req, res) {
+    axios.get("https://www.washingtonpost.com/opinions/?nid=top_nav_opinions&utm_term=.23f68f2edfd2").then(function (response) {
+        var $ = cheerio.load(response.data);
+        $(".story-list-story").each(function (i, element) {
+            var $el = $(element);
+
+            console.log("title: ", $el.children().eq(0).children().eq(1).children().eq(1).text()); //html());
+            console.log("link", $el.children().eq(1).children('a').attr('href'));
+            console.log("summary: ", $el.children().eq(0).children().eq(2).text()); //html());
+            console.log('---------')
+            console.log('---------')
+
+            var title = $(element).children('a').text();
+            var image = $(element).children('img').attr('src');
+            var summary = $(element).children('p').text();
+            var link = $(element).children('a').attr("href");
+
+            console.log(title, summary, link);
+
+            if (title && summary && link) {
+                db.scrapedData.insert({
+                    title: Title,
+                    summary: Summary,
+                    link: Link
+                },
+                function (err, inserted){
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(inserted);
+                    }
+                });
+            }
+        })
+    })
+    res.send("All Scraped!")
+});
+
+app.listen(3001, function () {
+    console.log("running on 3001");
 })
+
+
+
